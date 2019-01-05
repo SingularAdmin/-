2166e69ddfce083b1c9b4d749a61739f1755bef6
kostmol <kostmol@gmail.com>
2019-01-05 13:24:15 +0200

    quizForm

--- mchoice/mchoice/Form1.cs
+++ mchoice/mchoice/Form1.cs
@@ -39,5 +39,21 @@ namespace mchoice
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var newForm = new quizform();
+            newForm.Show();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
